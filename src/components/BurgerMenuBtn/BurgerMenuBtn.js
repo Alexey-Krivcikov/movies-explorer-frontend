7d5ff3834813a66bccc2d4ed5c8793794231bcb8
Alexey Krivchikov <krivchikovalexei@yandex.ru>
2023-07-11 22:49:51 +0300

    Исправил компонент BurgerMenuBtn

--- src/components/BurgerMenuBtn/BurgerMenuBtn.js
+++ src/components/BurgerMenuBtn/BurgerMenuBtn.js
@@ -1,13 +1,10 @@
 import './BurgerMenuBtn.css';
-import { useLocation } from 'react-router-dom';
 
 function BurgerMenuBtn({ isLoggedIn, onBurgerMenuOpen }) {
-  const { pathname } = useLocation();
-  const mainPath = pathname === '/';
   return (
     <button
       type='button'
-      className={`burger-menu-btn ${mainPath ? 'burger-menu-btn_type_main' : ''}  ${(!isLoggedIn) ? 'burger-menu-btn_is_hidden' : ''}`}
+      className={`burger-menu-btn ${(!isLoggedIn) ? 'burger-menu-btn_is-hidden' : ''}`}
       onClick={onBurgerMenuOpen}
     ></button>
   )
