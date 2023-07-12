8899ad3615345fecfb336ee808d086c67b1b5aa5
Alexey Krivchikov <krivchikovalexei@yandex.ru>
2023-07-12 16:10:36 +0300

    Сделал анимацию попапа

--- src/components/BurgerMenu/BurgerMenu.js
+++ src/components/BurgerMenu/BurgerMenu.js
@@ -9,7 +9,7 @@ function BurgerMenu({ onNavigateToProfile, isOpen, onClose }) {
     `burger-menu__link ${isActive ? 'burger-menu__link_active' : ''}`;
   return (
     <Popup isOpen={isOpen}>
-      <nav className='burger-menu'>
+      <nav className={`burger-menu ${isOpen ? 'burger-menu_is-open' : ''}`}>
         <button onClick={onClose} className='burger-menu__close-btn'></button>
         <ul className='burger-menu__nav'>
           <li className='burger-menu__nav-item'>
